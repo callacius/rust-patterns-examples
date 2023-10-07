80eef86e88d542de1371a0e77b94d682dd854e87
Daniel Costa <callacius@outlook.com>
2023-10-07 23:34:52 +0200

    *

---
+++ src/creational/mod.rs
@@ -0,0 +1,5 @@
+pub mod singleton;
+pub mod builder;
+pub mod prototype;
+pub mod factory_method;
+pub mod abstract_factory;
--- target/debug/deps/rust_patterns_examples-175ffbdb9f7813a4.d
+++ target/debug/deps/rust_patterns_examples-175ffbdb9f7813a4.d
@@ -1,8 +1,13 @@
-/Users/daniel/Documents/projetos/rust-patterns-examples/target/debug/deps/rust_patterns_examples-175ffbdb9f7813a4.rmeta: src/main.rs src/creational.rs src/structural.rs src/behavioral.rs
+/Users/daniel/Documents/projetos/rust-patterns-examples/target/debug/deps/rust_patterns_examples-175ffbdb9f7813a4.rmeta: src/main.rs src/creational/mod.rs src/creational/singleton.rs src/creational/builder.rs src/creational/prototype.rs src/creational/factory_method.rs src/creational/abstract_factory.rs src/structural/mod.rs src/behavioral/mod.rs
 
-/Users/daniel/Documents/projetos/rust-patterns-examples/target/debug/deps/rust_patterns_examples-175ffbdb9f7813a4.d: src/main.rs src/creational.rs src/structural.rs src/behavioral.rs
+/Users/daniel/Documents/projetos/rust-patterns-examples/target/debug/deps/rust_patterns_examples-175ffbdb9f7813a4.d: src/main.rs src/creational/mod.rs src/creational/singleton.rs src/creational/builder.rs src/creational/prototype.rs src/creational/factory_method.rs src/creational/abstract_factory.rs src/structural/mod.rs src/behavioral/mod.rs
 
 src/main.rs:
-src/creational.rs:
-src/structural.rs:
-src/behavioral.rs:
+src/creational/mod.rs:
+src/creational/singleton.rs:
+src/creational/builder.rs:
+src/creational/prototype.rs:
+src/creational/factory_method.rs:
+src/creational/abstract_factory.rs:
+src/structural/mod.rs:
+src/behavioral/mod.rs:
