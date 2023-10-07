9e7cdf0e803360acbcde9448b355cb17e433dfaa
Daniel Costa <callacius@outlook.com>
2023-10-07 23:38:06 +0200

    *

---
+++ target/debug/deps/lazy_static-b0539b9124e23f53.d
@@ -0,0 +1,6 @@
+/Users/daniel/Documents/projetos/rust-patterns-examples/target/debug/deps/lazy_static-b0539b9124e23f53.rmeta: /Users/daniel/.cargo/registry/src/index.crates.io-6f17d22bba15001f/lazy_static-1.4.0/src/lib.rs /Users/daniel/.cargo/registry/src/index.crates.io-6f17d22bba15001f/lazy_static-1.4.0/src/inline_lazy.rs
+
+/Users/daniel/Documents/projetos/rust-patterns-examples/target/debug/deps/lazy_static-b0539b9124e23f53.d: /Users/daniel/.cargo/registry/src/index.crates.io-6f17d22bba15001f/lazy_static-1.4.0/src/lib.rs /Users/daniel/.cargo/registry/src/index.crates.io-6f17d22bba15001f/lazy_static-1.4.0/src/inline_lazy.rs
+
+/Users/daniel/.cargo/registry/src/index.crates.io-6f17d22bba15001f/lazy_static-1.4.0/src/lib.rs:
+/Users/daniel/.cargo/registry/src/index.crates.io-6f17d22bba15001f/lazy_static-1.4.0/src/inline_lazy.rs:
